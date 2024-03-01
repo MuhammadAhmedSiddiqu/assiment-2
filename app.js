d11e76b110d221ac816e8cf8cd62491cd8c48550
MuhammadAhmedSiddiqu <ahmedsiddiqui461@gmail.com>
2024-03-02 04:18:56 +0500

    all files

---
+++ app.js
@@ -0,0 +1,344 @@
+// Chapter #4
+ 
+
+// Q#1
+// var myName = ("ahmed ")
+// var age = ("16 ")
+// var job = ("10th ")
+
+// alert ( myName +age +job )
+
+
+// Q#2
+// var myName = ("")
+// var myAge = ("")
+// var myHobbie = ("")
+// var myProffional = ("")
+// var myFatherName = ("")
+
+// *ILLEGALS NAME
+
+// var console = ("")
+// var name = ("")
+// var document = ("")
+// var source = ("")
+// var let = ("")// *LEGALS NAME
+
+
+// A)
+// var myheading = ("<h1> Rules for naming JS variables </h1>")
+// document.write (myheading)
+
+// B)
+// var myName = ("areeb ")
+// var age = ("27 ")
+// var hand = ("right ")
+// var professional = ("hr officer")
+// document.write (myName +age +hand +professional )
+
+
+// C)
+// var $myName = ("ahmed ")
+// var _myAge = ("16 ")
+// var myHobbie = ("cricket ")
+
+// document.write($myName+ _myAge+  myHobbie )
+
+// D)
+// var myNames = ("M.AHMED")
+// alert ("myNames")
+
+// END
+
+
+
+
+
+// CHAPTER#2
+
+// Q#1
+// var num1 = (8)
+// var num2 = (8)
+// var add = (num1+num2)
+// document.write(add)
+
+// Q#2
+// var num1 = (10)
+// var num2 = (8)
+// var sub = (num1-num2)
+// document.write(sub)
+
+// var num1 = (10)
+// var num2 = (4)
+// var modulas = (num1%num2)
+// document.write(modulas)
+
+// FOR SUB
+// var num1=(10);
+// var num2=(2);
+// var sub=(num1-num2);
+// document.write(sub)
+
+// FOR MUL
+// var num1=(7);
+// var num2=(8);
+// var  mul=(num1*num2);
+// document.write(mul)
+
+// // FOR DIV
+// var num1=(100);
+// var num2=(2);
+// var divide=(num1/num2);
+// document.write(divide;)
+
+// QUESTION #03
+// A)
+// var num=(2);
+// b)
+// document.write(num);
+// c)
+// num=(num+3)
+// d)
+// document.write(num);
+// e)
+// num=(++num)
+// f)
+// document.write(num)
+// g
+// num=(num+7);
+// h
+// document.write(num)
+// i
+// num=(--num);
+// j
+// document.write(num);
+// k
+// num=(num/4);
+// document.write(num)
+// l
+// document.write("reminder is zero 0")
+
+// // QUESTION #04
+// var perTicketValue=(600)
+// var result=(perTicketValue*5)
+// document.write("TOTAL COST TO BUY 5 TICKETS TO A MOVIE  "+ "<b>"+result+" PKR"+"</b>" )
+
+// QUESTION #5
+// document.write("Table of 4"
+// )
+// var tab=(4)
+// document.write(" <br>4*1="+tab)
+
+// tab=(4+4)
+// document.write(" <br>4*2="+tab)
+
+
+// tab=(tab+4)
+// document.write(" <br>4*3="+tab)
+
+// tab=(tab+4)
+// document.write(" <br>4*4="+tab)
+
+
+// tab=(tab+4)
+// document.write(" <br>4*5="+tab)
+
+
+// tab=(tab+4)
+// document.write(" <br>4*6="+tab)
+
+
+// tab=(tab+4)
+// document.write(" <br>4*7="+tab)
+
+
+// tab=(tab+4)
+// document.write(" <br>4*8="+tab)
+
+// tab=(tab+4)
+// document.write(" <br>4*9="+tab)
+
+// tab=(tab+4)
+// document.write(" <br>4*10="+tab)
+
+// QUESTION #06
+
+
+// var celsius = (70-32)*5/9;
+// var fahrenheit = (25 * 9/5) + 32;
+// document.write("20°C is " + fahrenheit + "°F")
+// document.write("<br>70°F is " + celsius + "°C")
+
+
+// QUESTION #07
+
+// var item1=(650);
+// var quantityOfItmem1=(3);
+// var item2=(100);
+// var quantityOfItmem2=(7);
+// var shippingCharges=(100);
+// var price1 =(item1*quantityOfItmem1)
+// var price2 =(item2*quantityOfItmem2)
+// var  TOTAL=(price1+price2+shippingCharges)
+
+// document.write("<b>"+"SHOPPING CART"+"</b>")
+// document.write("<br></br>")
+// document.write("<br> Price of 1st item "+ item1);
+// document.write("<br>Quantity of item 1 is "+ quantityOfItmem1);
+// document.write("<br> Price of 2nd item "+ item2);
+// document.write("<br>Quantity of item 2 is "+ quantityOfItmem2);
+// document.write("<br>  Shipping Charges "+shippingCharges);
+// document.write("<br>  Total Charges "+ "="+ TOTAL);
+
+// QUESTION #08
+// var totalMarks=(980);
+// var marksobtained=(804);
+// var percent=(marksobtained/9.8);
+// document.write("<b>"+"MARK SHEET"+"</b>");
+// document.write("<br> TOTAL MARKS= "+totalMarks);
+// document.write("<br> MARKS OBTAINED= "+marksobtained);
+// document.write("<br> Percentage= "+percent+"%");
+
+// QUESTION #09
+// var usdollar1=(104.80);
+// var saudirayal1=(28);
+
+// var totalCurrency=((usdollar1*10)+(saudirayal1*25));
+// document.write("TOTAL CURRENCY="+ totalCurrency)
+
+// QUESTION #10
+// var num=(10);
+// var result=(num*10/2);
+// document.write(result)
+
+// QUESTION #11
+// var currentYear=(2024);
+// var birthYear=(2006);
+
+// var aGE=(currentYear-birthYear);
+// document.write( "<b>Age calculator</b>");
+// document.write( "<br>Current year="+currentYear);
+// document.write( "<br> Birth year="+birthYear);
+// document.write("<br> AGE= "+aGE);
+
+// QUESTION #12
+// var r=(5);
+// var circuference=(3.142);
+// var ariaOfCircle=((circuference)*(r*r));
+// document.write("<b>The Geometrizer </b>");
+// document.write("<br>Radius of a circle:"+r);
+// document.write("<br>circuference of a circle:"+circuference);
+// document.write("<br>The Aria is:"+ariaOfCircle);
+
+// // QUESTION #13
+// var  currentAge=(30);
+// var maximumage=(80);
+// var perdaycconsume=(1);
+// var dayinayear=(365);
+// var remainingyears=(maximumage-currentAge);
+// var totalDaysRemaining=(remainingyears*dayinayear);
+// var totalSnacksConsume=(totalDaysRemaining*perdaycconsume);
+// document.write("<b> The Lifetime Supply Calculator</b>");
+// document.write("<br>Favourite snacks: chocolate chip");
+// document.write("<br>Current Age:"+currentAge);
+// document.write("<br>Estimited Maximum Age:"+maximumage);
+// document.write("<br>Amount of Snacks per day:"+perdaycconsume);
+// document.write("<br>You will need "+ totalSnacksConsume +" chocolate chips to last you until the ripe old age of "+ maximumage);
+
+// CHAPTER #06
+
+// // QUESTION #01
+// var num=(10);
+// document.write("The value of a is ="+num)
+// document.write("<br>The value of ++a is ="+ ++num);
+// document.write("<br> Now the value of ++a is ="+num);
+// document.write("<br>")
+// document.write("<br> The value of a++ is ="+num++);
+// document.write("<br> Now the value of a++ is ="+num);
+// document.write("<br>")
+// document.write("<br> the value of --a is ="+ --num);
+// document.write("<br>Now the value of a is ="+ num);
+// document.write("<br>")
+// document.write("<br> the value of a-- is = "+ num--);
+// document.write("<br> the value of a is ="+ num);
+
+
+// QUESTION #02
+// var a=(2);
+// var b=(1);
+// (--a);//now a is 1;
+// (--a - --b);//now a 0 & and b is 0
+// (--a - --b)+(++b);now (-1 -1)+(1);
+// (--a - --b)+(++b) +(b--);now (-2 - -2)+(2)+(1)
+// // then
+// -2 + 2 + 2 + 2 =4
+
+// document.write("a will be= 2")
+// document.write("<br> b will be= 1")
+// document.write("<br> RESULT=4")
+
+// QUESTION #03
+// var naMe= prompt("Areeb zaidi")
+
+// var naMe = prompt("Please enter your name:");
+
+//     document.write("Hello, " + naMe + "! Welcome to our program.");
+
+
+// QUESTION #06
+
+// var english=("English");
+// var urdu=("Urdu");
+// var math=("Math");
+
+
+// var totaleng=(100);
+// var totalurd=(100);
+// var totalmath=(100);
+
+// var obteng=(54);
+// var obturd=(78);
+// var obtmath=(48);
+// var per=((obteng+obtmath+obturd)*(100)/ (totaleng+totalmath+totalurd));
+
+// document.write("<table >");
+
+// document.write("<b>"+"Subject total marks Obtained marks Percentage"+  "</b>");
+// document.write("<tr>");
+// document.write("<td>"+"<b>"+"Subjects"+"</b>"+ "</td>");
+// document.write("<td>"+"<b>"+"Total Marks"+"</b>"+ "</td>");
+// document.write("<td>"+"<b>"+"Obtained Marks"+"</b>"+ "</td>");
+// document.write("<td>"+"<b>"+"Percentage"+"</b>"+ "</td>");
+// document.write("</tr>");
+
+
+// document.write("</table");
+
+// document.write("<table>");
+// document.write("<tr>");
+// document.write("<td>" +english  +"</td>");
+// document.write("<td>" +totaleng  +"</td>");
+// document.write("<td>" +obteng  +"</td>");
+// document.write("<td>" +obteng*100/totaleng+"%"  +"</td>");
+// document.write("</tr>");
+
+
+// document.write("<tr>");
+// document.write("<td>" +urdu  +"</td>");
+// document.write("<td>" +totalurd  +"</td>");
+// document.write("<td>" +obturd  +"</td>");
+// document.write("<td>" +obturd*100/totalurd+"%"  +"</td>");
+// document.write("</tr>");
+
+// document.write("<tr>");
+// document.write("<td>" +math  +"</td>");
+// document.write("<td>" +totalmath  +"</td>");
+// document.write("<td>" +obtmath  +"</td>");
+// document.write("<td>" +obtmath*100/totalmath+"%"  +"</td>");
+// document.write("</tr>");
+
+// document.write("<tr>");
+// document.write("<td>"+"</td>");
+// document.write("<td>"+(totaleng+totalmath+totalurd) +"</td>");
+// document.write("<td>"+(obteng+obturd+obtmath) +"</td>");
+// document.write("<td>"+per+ "%"+"</td>");
